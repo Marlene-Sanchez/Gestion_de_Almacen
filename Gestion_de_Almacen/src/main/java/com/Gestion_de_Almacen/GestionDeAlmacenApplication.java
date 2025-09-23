b6b185bb5f71103d516da7db7b26ff4914d6383b
Marlene-Sanchez <marlene_sanchez@anahuac.mx>
2025-09-22 20:04:50 -0600

    merge

--- Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/GestionDeAlmacenApplication.java
+++ Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/GestionDeAlmacenApplication.java
@@ -3,6 +3,7 @@ package com.Gestion_de_Almacen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GestionDeAlmacenApplication {
 
