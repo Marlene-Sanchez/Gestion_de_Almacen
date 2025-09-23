b838fb47fc6393bf6eadcfb62051c7ab1ecdb3fe
Marlene-Sanchez <marlene_sanchez@anahuac.mx>
2025-09-22 22:20:03 -0600

    merge

--- Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/PaginaController.java
+++ Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/PaginaController.java
@@ -2,6 +2,10 @@ package com.Gestion_de_Almacen;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PaginaController {
@@ -10,7 +14,10 @@ public class PaginaController {
     public String agregarProducto() {
         return "producto";
     }
-
+    @GetMapping("/Search")
+    public String mostrarSearch() {
+        return "Search";
+    }
     @GetMapping("/Login")
     public String mostrarLogin() {
         return "Login";
@@ -19,4 +26,18 @@ public class PaginaController {
     public String mostrarInicio() {
         return "dashboard";
     }
+    @PostMapping("/procesarLogin")
+    public String procesarLogin(@RequestParam String usuario,
+                                @RequestParam String password,
+                                RedirectAttributes redirectAttributes) {
+
+        if (usuario.equals("admin") && password.equals("1234")) {
+            redirectAttributes.addFlashAttribute("nombreUsuario", usuario);
+            return "redirect:/Dashboard";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Usuario o contraseña incorrectos");
+            return "redirect:/Login";
+        }
+    }
+
 }
