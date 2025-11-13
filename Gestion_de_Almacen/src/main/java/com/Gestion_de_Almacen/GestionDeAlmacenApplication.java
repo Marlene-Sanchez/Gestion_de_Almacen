9d170f6725397cdb96aaf271784d4db05b78ad67
Marlene-Sanchez <marlene_sanchez@anahuac.mx>
2025-11-12 20:30:03 -0600

    Se cambiaron a mayúsculas los atributos de los datos insertados

--- Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/GestionDeAlmacenApplication.java
+++ Gestion_de_Almacen/src/main/java/com/Gestion_de_Almacen/GestionDeAlmacenApplication.java
@@ -44,36 +44,36 @@ public class GestionDeAlmacenApplication {
                 //ADIDAS CAMPUS
                 float[] tallasCampusGris = {23.5F, 24, 24.5F, 25, 25.5F, 26};
                 for (float talla : tallasCampusGris) {
-                    tenisList.add(new Tenis(adidas, "Campus", talla, "Gris/Negro/Blanco", 450.0, 6));
+                    tenisList.add(new Tenis(adidas, "CAMPUS", talla, "GRIS/NEGRO/BLANCO", 450.0, 6));
                 }
 
                 float[] tallasCampusNegro = {25.5F, 26, 26.5F, 27, 27.5F, 28};
                 for (float talla : tallasCampusNegro) {
-                    tenisList.add(new Tenis(adidas, "Campus", talla, "Negro", 450.0, 6));
+                    tenisList.add(new Tenis(adidas, "CAMPUS", talla, "NEGRO", 450.0, 6));
                 }
 
                 //ADIDAS CONCHA
                 float[] tallasConcha = {25.5F, 26, 26.5F, 27, 27.5F, 28.5F};
                 for (float talla : tallasConcha) {
-                    tenisList.add(new Tenis(adidas, "Concha", talla, "Blanco", 400.0, 6));
+                    tenisList.add(new Tenis(adidas, "CONCHA", talla, "BLANCO", 400.0, 6));
                 }
 
                 //ADIDAS SUPER-NOVA
                 float[] tallasspn = {25, 26, 26.5F, 27, 27.5F, 28};
                 for (float talla : tallasspn) {
-                    tenisList.add(new Tenis(adidas, "Super-Nova", talla, "Blanco/Negro", 450.0, 6));
+                    tenisList.add(new Tenis(adidas, "SUPER-NOVA", talla, "BLANCO/NEGRO", 450.0, 6));
                 }
 
                 //VANS 1981
                 float[] tallasvans1981 = {26.5F, 27, 27.5F, 29, 28.5F,29.5F};
                 for (float talla : tallasvans1981) {
-                    tenisList.add(new Tenis(vans, "1981", talla, "Negro/Blanco", 400.0, 6));
+                    tenisList.add(new Tenis(vans, "1981", talla, "NEGRO/BLANCO", 400.0, 6));
                 }
 
                 //JORDAN RETRO 4
                 float[] tallasjrretro = {25.5F, 26, 26.5F, 27, 27.5F, 28.5F};
                 for (float talla : tallasjrretro) {
-                    tenisList.add(new Tenis(jordan, "Retro 4", talla, "Blanco/Gris/Verde", 450.0, 6));
+                    tenisList.add(new Tenis(jordan, "RETRO 4", talla, "BLANCO/GRIS/VERDE", 450.0, 6));
                 }
 
                 tenisRepo.saveAll(tenisList);
